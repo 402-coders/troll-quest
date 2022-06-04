3381edb60c39781e77b746275ebea8af48dca39b
Szymon Kwak <szymon.kwak@gmail.com>
2022-06-05 01:12:59 +0200

    favicon and logos

--- src/components/shared/components/NavBar/NavBar.tsx
+++ src/components/shared/components/NavBar/NavBar.tsx
@@ -3,7 +3,9 @@ import { useAuthState } from '~/components/contexts/UserContext';
 import { RequireAuth } from '~/components/domain/auth/RequireAuth';
 import { useDefaultTheme } from '~/components/domain/shop/hooks/useDefaultTheme';
 import { appRoutes } from '../../../router/appRoutes';
-import UserInfo from './UserInfo';
+import { UserInfo } from './UserInfo';
+import trollHead from '~/assets/troll/head.png';
+import logo from '~/assets/Logo-horizontal.png';
 
 function NavBar() {
   const { user } = useAuthState();
@@ -12,8 +14,10 @@ function NavBar() {
   return (
     <RequireAuth>
       <>
-        <nav className="navbar h-20 bg-primary justify-between px-6 mb-8">
+        <nav className="navbar h-20 bg-primary justify-between px-6">
           <div className="gap-4">
+            <img src={trollHead} alt="troll head" className="w-16" />
+            <img src={logo} alt="troll head" className="w-24 mr-4" />
             <Link to={appRoutes.home} className="btn btn-ghost normal-case text-xl">
               Start
             </Link>
