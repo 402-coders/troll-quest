e924ddaa67e5c777560334c3271f5b2b9bc4dca6
Szymon Kwak <szymon.kwak@gmail.com>
2022-06-05 00:28:56 +0200

    fix loader

--- src/components/shared/components/Loader.tsx
+++ src/components/shared/components/Loader.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import trollRun from '~/assets/troll/troll-run.gif';
 
 export const Loader = () => {
   return (
-    <img src="../../../../assets/walk-troll.gif" />
-  )
-}
+    <div className=" w-screen h-screen pt-20 flex flex-col bg-secondary items-center ">
+      <h1 className="my-8 text-3xl font-bold">Wczytuję ...</h1>
+      <img src={trollRun} alt="walking troll" className="w-4/12" />
+    </div>
+  );
+};
