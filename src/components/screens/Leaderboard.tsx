bf3e072e926c88e21f1178bcdb26fdb390c04077
Marharyta Leśniak <sirachenko944@gmail.com>
2022-06-05 01:57:16 +0200

    add heroes

--- src/components/screens/Leaderboard.tsx
+++ src/components/screens/Leaderboard.tsx
@@ -2,29 +2,30 @@ import { collection, getFirestore, query } from 'firebase/firestore'
 import React from 'react'
 import {useCollectionData} from 'react-firebase-hooks/firestore'
 import { firebaseApp } from '~/lib/firebase'
+import { heroImage } from '../domain/select-mode/utils/heroTypes'
 import GoldGlass from '../shared/components/GoldGlass'
 import { Header } from '../shared/components/Header'
 
 export const Leaderboard = () => {
     const firestore = getFirestore(firebaseApp);
     const [users]  = useCollectionData(query(collection(firestore, 'users')))
-    console.log(users);
     
   return (
-    <div>
+    <div className="p-8">
         <Header>Leaderboard</Header>
         <div className="flex flex-col items-center">
             {users?.map((user, idx )=> (
                 <div key={idx} className="mt-10 card w-1/2 h-20 shadow-sm flex flex-row justify-around items-center border-2">
                    <div className="avatar self-center">
-        <div className="w-12 h-12 rounded-full ring ring-accent">
-          <img src={user.avatar} alt="user avatar" />
+                <div className="w-12 h-12 rounded-full">
+                <img src={user.avatar} alt="user avatar" />
+            </div>
         </div>
-      </div>
-                <p>{user.displayName}</p>
-                {user.currentHero}
+                <p className="w-5">{user.displayName}</p>
+                <img src={heroImage[user.currentHero]} alt="user hero" className="w-20" />
+                
                 <div className="flex">
-                    <p className="m-2">{user.points}</p>
+                    <p className="m-2 text-lg font-black">{user.points}</p>
                     <GoldGlass />
                 </div>
             </div>
