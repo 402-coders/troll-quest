ea70668346b1e1234af007c5434bf556fc87d759
Marcin Woś <marcin.wos@coderscrew.pl>
2022-06-04 13:41:09 +0200

    fix shit

--- src/components/screens/Index.tsx
+++ src/components/screens/Index.tsx
@@ -1,10 +1,4 @@
-// import { Dialog } from '@headlessui/react';
-// import { useRef, useState } from 'react';
-// import { useAuthState } from '~/components/contexts/UserContext';
-// import { SignInButton } from '~/components/domain/auth/SignInButton';
-// import { SignOutButton } from '~/components/domain/auth/SignOutButton';
-// import { Head } from '~/components/shared/Head';
-import { GoogleButton } from '../shared/components/GoogleButton/GoogleButton';
+import { GoogleButton } from '../domain/auth/components/GoogleButton';
 import Logo from '../shared/components/Logo/Logo';
 import { ShareButton } from '../shared/components/ShareButton/ShareButton';
 
@@ -17,9 +11,11 @@ function Index() {
       {/* <Head title="TOP PAGE" /> */}
       <div className="w-screen h-screen grid grid-cols-2 grid-rows-1 min-h-screen bg-secondary m-0">
         <img className="object-cover h-full w-full" src="../../../assets/main-page-troll.gif" alt="" />
-        <div className="flex flex-col justify-center items-center"><Logo />
-        <GoogleButton />
-        <ShareButton /></div>    
+        <div className="flex flex-col justify-center items-center">
+          <Logo />
+          <GoogleButton />
+          <ShareButton />
+        </div>
       </div>
     </>
   );
