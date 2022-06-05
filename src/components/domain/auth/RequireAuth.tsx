951f498163fcfaf3b75a922fef6f0a065f521477
Marcin Woś <marcin.wos@coderscrew.pl>
2022-06-05 05:32:28 +0200

    działa nie pytam

--- src/components/domain/auth/RequireAuth.tsx
+++ src/components/domain/auth/RequireAuth.tsx
@@ -2,6 +2,7 @@ import { ReactElement } from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuthState } from '~/components/contexts/UserContext';
 import { appRoutes } from '~/components/router/appRoutes';
+import Index from '~/components/screens/Index';
 import { Loader } from '~/components/shared/components/Loader';
 // import Page404 from '~/components/screens/404/404';
 
@@ -11,10 +12,11 @@ type RequireAuthProps = {
 
 const RequireAuth = ({ children }: RequireAuthProps) => {
   const { state } = useAuthState();
+  console.log('🚀 ~ file: RequireAuth.tsx ~ line 14 ~ RequireAuth ~ state', state.state);
 
-  if (state.state === 'SIGNED_OUT') return <Navigate to={appRoutes.signIn} />;
+  if (state.state === 'SIGNED_OUT') return <Index />;
   if (state.state === 'UNKNOWN') return <Loader />;
-  return state.state === 'SIGNED_IN' ? children : <Navigate to={appRoutes.signIn} />;
+  return state.state === 'SIGNED_IN' ? children : <Navigate to={appRoutes.home} />;
 };
 
 export { RequireAuth };
