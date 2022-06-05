cf2123802a7144ab32dac983f1ca2dbcf205cd33
Szymon Kwak <szymon.kwak@gmail.com>
2022-06-05 08:30:20 +0200

    user hero in select page

--- src/components/domain/select-mode/components/SelectMode.tsx
+++ src/components/domain/select-mode/components/SelectMode.tsx
@@ -7,22 +7,24 @@ import ModeCard from './ModeCard';
 import Party from './modes/Party';
 import SinglePlayer from './modes/SinglePlayer';
 import TwoPlayers from './modes/TwoPlayers';
+import { useAuthState } from '~/components/contexts/UserContext';
 
 const SelectMode = () => {
+  const { user } = useAuthState();
   return (
     <>
       <Head title={appRoutesHeaders.selectGame} />
       <div className="flex h-5/6 flex-wrap justify-center content-center m-auto max-w-screen-xl">
         <div className="flex">
           <ModeCard>
-            <SinglePlayer hero={heroImage['elf']} />
+            <SinglePlayer hero={user ? heroImage[user.currentHero] : heroImage.elf} />
           </ModeCard>
           <ModeCard>
-            <TwoPlayers hero1={heroImage.elf} hero2={heroImage.knight} />
+            <TwoPlayers hero1={user ? heroImage[user.currentHero] : heroImage.elf} hero2={heroImage.knight} />
           </ModeCard>
         </div>
         <ModeCard>
-          <Party hero1={heroImage.elf} hero2={heroImage.knight} hero3={heroImage.warrior} />
+          <Party hero1={heroImage.elf} hero2={heroImage.warrior} hero3={heroImage.knight} />
         </ModeCard>
       </div>
     </>
