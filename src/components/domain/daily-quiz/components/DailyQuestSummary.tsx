e948f21f658cf037a173a631f7a32bcc6b329044
Szymon Kwak <szymon.kwak@gmail.com>
2022-06-05 03:58:45 +0200

    party mode carousel

--- src/components/domain/daily-quiz/components/DailyQuestSummary.tsx
+++ src/components/domain/daily-quiz/components/DailyQuestSummary.tsx
@@ -9,7 +9,7 @@ export const DailyQuestSummary = () => {
   const totalScore = questions.reduce((acc, current) => (current.isCorrect ? acc + 1 : acc), 0);
 
   return (
-    <div className="justify-content flex-col flex w-100 mt-8">
+    <div className="justify-content flex-col flex w-100 m-8">
       <Header>Odpowiedziałeś poprawnie na: {totalScore}</Header>
       <span className="text-center text-2xl my-4">Dostajesz {totalScore * 10} złotych lup</span>
       <div className="flex gap-4 justify-evenly w-full mt-5">
